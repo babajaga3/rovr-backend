fa8376301ab249c2b1b3fc14e0f7a68862fe1479
Toma Bourov <t.bourov@gmail.com>
2024-01-04 11:50:20 +0200

    :mag: Lint

--- index.ts
+++ index.ts
@@ -1,7 +1,7 @@
 import cors from 'cors'
+import 'dotenv/config'
 import express from 'express'
 import morgan from 'morgan'
-import 'dotenv/config'
 
 const app = express()
 const PORT = process.env.PORT ?? 8080
