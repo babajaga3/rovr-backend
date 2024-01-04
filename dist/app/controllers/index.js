2313f343de52a6c3161622bc54e3c0c8299c11bb
Toma Bourov <t.bourov@gmail.com>
2024-01-04 12:36:19 +0200

    Revert "Revert ":construction: Add dist""
    This reverts commit df4bcc223e987f3eecc087ac7c5352f2b1eba34a.

---
+++ dist/app/controllers/index.js
@@ -0,0 +1,7 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.getStationData = exports.getStations = void 0;
+var getStations_1 = require("./getStations");
+Object.defineProperty(exports, "getStations", { enumerable: true, get: function () { return getStations_1.getStations; } });
+var getStationData_1 = require("./getStationData");
+Object.defineProperty(exports, "getStationData", { enumerable: true, get: function () { return getStationData_1.getStationData; } });
