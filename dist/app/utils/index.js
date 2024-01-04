488bfd6d92ca2c3c16bfc3df47922d917f32de59
Toma Bourov <t.bourov@gmail.com>
2024-01-04 12:32:20 +0200

    :construction: Add dist

---
+++ dist/app/utils/index.js
@@ -0,0 +1,7 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.extractStations = exports.extractDepartures = void 0;
+var extractDepartures_1 = require("./extractDepartures");
+Object.defineProperty(exports, "extractDepartures", { enumerable: true, get: function () { return extractDepartures_1.extractDepartures; } });
+var extractStations_1 = require("./extractStations");
+Object.defineProperty(exports, "extractStations", { enumerable: true, get: function () { return extractStations_1.extractStations; } });
