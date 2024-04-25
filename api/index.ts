3775b3975bf381e095f8a83ec30e5f4e2ba0b3ee
Toma Bourov <t.bourov@gmail.com>
2024-04-25 09:53:36 +0300

    :zap: Finally works(?)

--- api/index.ts
+++ api/index.ts
@@ -6,7 +6,7 @@ import morgan from 'morgan'
 const app = express()
 const PORT = process.env.PORT ?? 8080
 
-import routes from './app/routes'
+import routes from '../app/routes'
 
 app.use(cors())
 app.use(morgan('dev'))
